72b2fb9e66dfb7e470cb7a3fbc503c7a75b3c2cc
Sunniva Indrehus <sunniva.indrehus@gmail.com>
2021-04-30 12:04:55 +0200

    (:bug:) Removed output folder

--- output/PA-SLZ.siterun.fates.docker.I2000Clm50BgcCrop.Cd08b750-F5534a94.2021-04-28_14-22-27/.env_mach_specific.csh
+++
@@ -1,6 +0,0 @@
-# This file is for user convenience only and is not used by the model
-# Changes to this file will be ignored and overwritten
-# Changes to the environment should be made in env_mach_specific.xml
-# Run ./case.setup --reset to regenerate this file
-setenv HDF5_HOME /usr/local/hdf5
-setenv NETCDF_PATH /usr/local/netcdf
--- output/PA-SLZ.siterun.fates.docker.I2000Clm50BgcCrop.Cd08b750-F5534a94.2021-04-28_14-29-11/.env_mach_specific.sh
+++
@@ -1,6 +0,0 @@
-# This file is for user convenience only and is not used by the model
-# Changes to this file will be ignored and overwritten
-# Changes to the environment should be made in env_mach_specific.xml
-# Run ./case.setup --reset to regenerate this file
-export HDF5_HOME=/usr/local/hdf5
-export NETCDF_PATH=/usr/local/netcdf
